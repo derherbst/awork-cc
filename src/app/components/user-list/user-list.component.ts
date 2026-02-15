600ae6273066fc67463d883b817fd1c3b95e7b14
Gerbst G <gerbst.g@gmail.com>
2026-02-15 22:23:22 +0100

    added virtual scroll back

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, input, output, ChangeDetectionStrategy } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { UserItemComponent } from '../user-item/user-item.component';
 import { ListRow } from '../../services/grouping.service';
 import { CountryNamePipe } from '../../pipes/country-name.pipe';
 import { CountryFlagPipe } from '../../pipes/country-flag.pipe';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss',
-  imports: [UserItemComponent, CountryNamePipe, CountryFlagPipe],
+  imports: [UserItemComponent, ScrollingModule, CountryNamePipe, CountryFlagPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListComponent {
